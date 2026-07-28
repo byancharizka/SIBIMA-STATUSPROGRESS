62e66a524ad8cdbf7c741f92026b6b14229d4630
Byanca BI siBima <byancharizkamulyanti@gmail.com>
2026-07-28 09:49:47 +0700

    Add finction rename to transaction_date in data_new, add tuple

--- app.py
+++ app.py
@@ -370,18 +370,21 @@ def load_all_data(start_date=None, end_date=None) -> dict[str, pd.DataFrame]:
 def load_all_data_new(start_date=None, end_date=None) -> dict[str, pd.DataFrame]:
     # Mapping endpoint baru sesuai API kamu
     endpoint_map_new = {
-        "so": "sales-orders",
-        "pr": "purchase-requests",
-        "po": "purchase-orders",
-        "grn" : "goods-receipt-notes",
-        "do": "delivery-orders",
-        "si" : "sales-invoices"
-        #"npr": "purchase-requests",
+        "so": ("sales-orders", {"date" : "transaction_date"}),
+        "pr": ("purchase-requests",{}),
+        "po": ("purchase-orders", {"date" : "transaction_date"}),
+        "grn" : ("goods-receipt-notes", {}),
+        "do": ("delivery-orders",{}),
+        "si" : ("sales-invoices",{})
     }
 
     result_new = {}
-    for key, endpoint in endpoint_map_new.items():
+    for key, (endpoint, rename_map_new) in endpoint_map_new.items():
         df = get_api_data_new(endpoint, source="eas", start_date=start_date, end_date=end_date)
+
+        if not df.empty:
+            df = df.rename(columns=rename_map_new)
+            df = safe_to_datetime(df, "transaction_date")
         result_new[key] = df
 
     return result_new
